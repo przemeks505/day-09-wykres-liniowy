26a7f1c82a8c95b36293827e2efc10243a66452c
Przemek <przemeks505@gmail.com>
2023-12-28 12:41:09 +0100

    dodanie przykładu st.altair_chart

--- day-09-wykres-liniowy.py
+++ day-09-wykres-liniowy.py
@@ -16,6 +16,7 @@ Stworzenie i wyświetlenie wykresu liniowego przy użyciu polecenia st.line_char
 import streamlit as st
 import pandas as pd
 import numpy as np
+import altair as alt
 
 st.header('Wykres linowy')
 
@@ -24,3 +25,13 @@ chart_data = pd.DataFrame(
     columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+c = (
+   alt.Chart(chart_data)
+   .mark_circle()
+   .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+)
+
+st.altair_chart(c, use_container_width=True)
